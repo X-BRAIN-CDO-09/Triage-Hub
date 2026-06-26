82429848a4cb384f793e589240b866359dc5e26d
nguyenductien.qnm <ndtien317@gmail.com>
2026-06-26 11:33:16 +0700

    feat(eks): add access entries for team IAM users

--- capstone/tf-1/devops/infra/modules/eks/variables.tf
+++ capstone/tf-1/devops/infra/modules/eks/variables.tf
@@ -40,3 +40,9 @@ variable "public_access_cidrs" {
   type        = list(string)
   default     = ["0.0.0.0/0"]
 }
+
+variable "cluster_admin_arns" {
+  description = "Danh sách IAM User/Role ARN được cấp quyền admin vào EKS Kubernetes API"
+  type        = list(string)
+  default     = []
+}
