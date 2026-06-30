17481fa7bbdc673bd3e18c00c463faf3988e8504
nguyenductien.qnm <ndtien317@gmail.com>
2026-06-30 22:56:38 +0700

    feat(infra): migrate raw-alert and buffer queues to SQS FIFO

--- capstone/tf-1/devops/infra/modules/sqs/main.tf
+++ capstone/tf-1/devops/infra/modules/sqs/main.tf
@@ -6,8 +6,10 @@
 resource "aws_sqs_queue" "dlq" {
   for_each = var.queues
 
-  name                      = "${var.project_name}-${each.key}-dlq"
-  message_retention_seconds = 1209600 # 14 days (max retention for debugging)
+  name                        = "${var.project_name}-${each.key}-dlq${each.value.fifo_queue ? ".fifo" : ""}"
+  message_retention_seconds   = 1209600 # 14 days (max retention for debugging)
+  fifo_queue                  = each.value.fifo_queue
+  content_based_deduplication = each.value.fifo_queue ? each.value.content_based_deduplication : null
 
   tags = {
     Name = "${var.project_name}-${each.key}-dlq"
@@ -18,9 +20,11 @@ resource "aws_sqs_queue" "dlq" {
 resource "aws_sqs_queue" "this" {
   for_each = var.queues
 
-  name                       = "${var.project_name}-${each.key}"
-  visibility_timeout_seconds = each.value.visibility_timeout_seconds
-  message_retention_seconds  = each.value.message_retention_seconds
+  name                        = "${var.project_name}-${each.key}${each.value.fifo_queue ? ".fifo" : ""}"
+  visibility_timeout_seconds  = each.value.visibility_timeout_seconds
+  message_retention_seconds   = each.value.message_retention_seconds
+  fifo_queue                  = each.value.fifo_queue
+  content_based_deduplication = each.value.fifo_queue ? each.value.content_based_deduplication : null
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq[each.key].arn
--- capstone/tf-1/devops/infra/modules/sqs/variables.tf
+++ capstone/tf-1/devops/infra/modules/sqs/variables.tf
@@ -5,9 +5,11 @@ variable "project_name" {
 
 variable "queues" {
   type = map(object({
-    visibility_timeout_seconds = optional(number, 30)
-    message_retention_seconds  = optional(number, 345600) # 4 days
-    max_receive_count          = optional(number, 5)
+    visibility_timeout_seconds  = optional(number, 30)
+    message_retention_seconds   = optional(number, 345600) # 4 days
+    max_receive_count           = optional(number, 5)
+    fifo_queue                  = optional(bool, false)
+    content_based_deduplication = optional(bool, false)
   }))
   description = "Map of SQS queue configurations to create along with their DLQs"
 }
