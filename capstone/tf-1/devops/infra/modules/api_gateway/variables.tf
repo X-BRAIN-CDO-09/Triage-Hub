dc96e95204f7e6243ffeba49669499bfa85f9f72
TanHuy <kokoreina2005@gmail.com>
2026-06-30 20:35:06 +0700

    feat: route alert ingress through raw SQS queue

--- capstone/tf-1/devops/infra/modules/api_gateway/variables.tf
+++ capstone/tf-1/devops/infra/modules/api_gateway/variables.tf
@@ -14,11 +14,17 @@ variable "environment" {
 
 variable "integrations" {
   type = map(object({
-    path_part           = string
-    http_method         = string
-    lambda_function_arn = string
-    lambda_name         = string
-    api_key_required    = optional(bool, false)
+    path_part        = string
+    http_method      = string
+    api_key_required = optional(bool, false)
+
+    integration_type = optional(string, "lambda_proxy")
+
+    lambda_function_arn = optional(string)
+    lambda_name         = optional(string)
+
+    sqs_queue_arn  = optional(string)
+    sqs_queue_name = optional(string)
   }))
-  description = "Map of API Gateway endpoints and their target Lambda ARNs"
+  description = "Map of API Gateway endpoints and their integration targets"
 }
